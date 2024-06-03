22ba0b0b452efecfe61149e323f89e45ef7e3157
rubendc <rubendcdec@proton.me>
2024-06-03 12:24:58 +0200

    Ajout des premiers fichiers de configuration

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	devtools: { enabled: true },
+	modules: ['@nuxtjs/google-fonts', '@pinia/nuxt', 'nuxt-icon', "@vueuse/nuxt"],
+
+	googleFonts: {
+		families: {
+			Poppins: [400, 700],
+			Inter: '100..800',
+		},
+	},
+
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@use "~/assets/styles/_colors.scss" as *;',
+				},
+			},
+		},
+	},
+
+	css: ['@/assets/styles/index.scss'],
+});
