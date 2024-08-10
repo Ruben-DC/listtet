f2fb3ccfcfe6031f5803229ba6f8d359797c177b
rubendc <rubendcdev@proton.me>
2024-08-10 17:28:18 +0200

    Amélioration de l'affichage du feed

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,22 @@
 <template>
 	<div class="feed">
 		<TasksForm />
+
+		<TodoList />
 	</div>
 </template>
 
 <style lang="scss" scoped>
 	.feed {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: center;
+		gap: 30px 20px;
+
 		width: 100%;
-		max-width: 900px;
 		height: fit-content;
+
+		padding-bottom: 50px;
 	}
 </style>
